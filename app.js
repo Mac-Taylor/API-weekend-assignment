0c01290175a5105b207dd4659b84535dae5aaa18
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-22 13:36:19 -0400

    commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * JS file for controllers and services and components and all that. 
+ * 
+ */
+
+const app = angular.module('GitHubActivity', []);
+
+app.controller('UsernameController', function ($scope, AppService) {
+
+});
+
+app.controller('UsernameController', function ($scope, AppService) {
+
+});
+
+app.factory('AppService', function ($http) {
+
+}); 
