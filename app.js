06d4ab7ca657f495581babfe3574a05983cd12d1
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-23 09:03:24 -0400

    Tuesday 9:03

--- app.js
+++ app.js
@@ -7,17 +7,66 @@
 const app = angular.module('GitHubActivity', []);
 
 app.controller('UsernameController', function ($scope, AppService) {
-    $scope.
+    $scope.addUser = function () {
+        AppService.addNewUser($scope.username);
+        console.log('worked');
+    };
 });
 
-app.controller('UsernameController', function ($scope, AppService) {
 
+app.controller('UserActivityController', function ($scope, AppService) {
+
+    $scope.ppl = AppService.getUsers();
+    $scope.displayUser = function () {
+
+
+
+        // AppService.addNewUser();
+        // I want the UserActivityController section to display the username and the
+        // relevant data whenever I click to add that User. call the showActivity function
+
+    };
 });
 
 app.factory('AppService', function ($http) {
 
     const ppl = [];
-    $http.get('https://api.github.com/users/' + username + '/events')
 
-    
-}); 
+    return {
+
+        getUsers() {
+            return ppl;
+        },
+
+        addNewUser(username) {
+
+            let usr = { // make sure to be mindful of this object because you need a blank object when you are
+                        // trying to fill in the DOM you need data stored somewhere, ideally in one place rather than
+                name: username,
+                repositories: null,
+            }
+
+            ppl.push(usr);
+
+            $http.get('https://api.github.com/users/' + username).then(function (response) {
+                console.log(response.data);
+                usr.repositories = response.data.public_repos;
+                // // ppl.push({
+                //     name: username,
+                //     repositories: response.data.public_repos,
+                // });
+            });
+
+            $http.get('https://api.github.com/users/' + username + '/events').then(function (response) {
+                console.log(response);
+                for (let i = 0; i < response.data.length; i++) {
+                    console.log(response.data[i].created_at);  
+                }
+            });
+        },
+
+        showActivity() {
+
+        },
+    };
+});
